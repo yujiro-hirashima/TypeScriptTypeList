06869817c88e203ccc52f9e19e741023312c2c02
yujiro-hirashima <75685573+yujiro-hirashima@users.noreply.github.com>
2021-03-22 09:58:42 +0900

    Plactice! お勉強

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./styles.css";
-
+import { Practice1 } from "./plactices/Practice1";
+import { Practice2 } from "./plactices/Practice2";
+import { Practice3 } from "./plactices/Practice3";
+import { Practice4 } from "./plactices/Practice4";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Practice1 />
+      <Practice2 />
+      <Practice3 />
+      <Practice4 />
     </div>
   );
 }
